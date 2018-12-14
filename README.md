0b6e44c2f4aab6f8c19c86c723f5dd266f60d2d6
naoya <mine@mine-studio.com>
2018-12-14 18:03:21 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# How to run
+
+```
+ruby str.rb
+```
+
---
+++ str.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+
+def encode_and_write(str, encoding)
+  name = encoding.name
+  encoded_str = str.encode(encoding)
+  puts("%s: %d bytes" % [name, encoded_str.bytesize])
+  File.open("dump_%s.txt" % name, "w") do |f|
+    f.print(encoded_str)
+  end
+  File.open("hexdump_%s.txt" % name, "w") do |f|
+    encoded_str.bytes.each do |byte|
+      f.print(byte.to_s(16))
+    end
+  end
+end
+
+str = "iisecは、2019年1月26日に説明会を開催する。"
+encode_and_write(str, Encoding::ISO_2022_JP)
+encode_and_write(str, Encoding::Shift_JIS)
+encode_and_write(str, Encoding::EUC_JP)
+encode_and_write(str, Encoding::UTF_8)
+

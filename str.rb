1aafbcc69857bf5918d05fc5779ed90306204dc0
naoya <mine@mine-studio.com>
2018-12-20 11:16:01 +0900

    Write in binary mode

--- str.rb
+++ str.rb
@@ -1,13 +1,15 @@
 #!/usr/bin/env ruby
+#
+# Written by Naoya Niwa for report of IISEC lecture.
 
 def encode_and_write(str, encoding)
   name = encoding.name
   encoded_str = str.encode(encoding)
   puts("%s: %d bytes" % [name, encoded_str.bytesize])
-  File.open("dump_%s.txt" % name, "w") do |f|
+  File.open("dump_%s.txt" % name, "wb") do |f|
     f.print(encoded_str)
   end
-  File.open("hexdump_%s.txt" % name, "w") do |f|
+  File.open("hexdump_%s.txt" % name, "wb") do |f|
     encoded_str.bytes.each do |byte|
       f.print(byte.to_s(16))
     end
